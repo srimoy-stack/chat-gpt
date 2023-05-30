2190dabc7f3dbac205584783d65e4b57e1dbfdb2
srimoy-stack <das6370773567@gmail.com>
2023-05-30 11:51:28 +0530

    update in server

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const express = require("express");
-
 const cors = require("cors");
-
 const bodyParser = require("body-parser");
-
 const { Configuration, OpenAIApi } = require("openai");
 
 const config = new Configuration({
-  apiKey: "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",
+  apiKey: "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",
 });
 
 const openai = new OpenAIApi(config);
 
 // Setup server
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// endpoint for ChatGPT
-
+// Endpoint for ChatGPT
 app.post("/chat", async (req, res) => {
   const { prompt } = req.body;
 
@@ -32,7 +27,7 @@ app.post("/chat", async (req, res) => {
   res.send(completion.data.choices[0].text);
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
